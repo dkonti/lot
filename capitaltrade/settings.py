127c0375447090a4d11624d02005f7aeb4c27885
dkonti <richardmasondgreat@gmail.com>
2022-05-26 15:59:08 +0100

    database connection

--- capitaltrade/settings.py
+++ capitaltrade/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'capitaltrade.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'libertexdb',
+        'USER': 'libertex_admin',
+        'PASSWORD': 'mellykonti4',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

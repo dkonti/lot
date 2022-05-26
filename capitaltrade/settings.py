766428344df5e08eb7b53c5579a741e0820fc3a6
dkonti <richardmasondgreat@gmail.com>
2022-05-26 12:01:55 +0000

    please lord

--- capitaltrade/settings.py
+++ capitaltrade/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'capitaltrade.wsgi.application'
 
 # Database
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -89,7 +89,14 @@ DATABASES = {
         'PORT': '',
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
